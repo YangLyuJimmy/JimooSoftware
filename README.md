9be6abdf8ffb6911e08608fd22ac951ed1b9785a
YangLyuJimmy <jimmylyuyang@outlook.com>
2025-01-24 21:40:07 -0800

    Update website tab title and icon

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# JimooSoftware
+## JimooSoftware Company Official Website
+
+
+
+
+
+## Deployment
+
+# Files need to be deleted before '''npm run build'''
+favicon.png
+index.html
+manifest.json
+robots.txt
+asset-manifest.json
+static folder
+
+# To move files from build folder to root
+mv build/* .
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+const logo = require('./logo.png');
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} alt="Jimoo Software Logo" className="App-logo" />
         <h1>Welcome to Jimoo Software</h1>
       </header>
     </div>
